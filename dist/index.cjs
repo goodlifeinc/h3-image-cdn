5c5ad2a37501149207107970e119795e6e3c2f6f
goodlifeinc <genata93@gmail.com>
2024-11-03 16:59:09 +0200

    remove build

--- dist/index.cjs
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var h3 = require('h3');
-var listhen = require('listhen');
-var app = require('./app.cjs');
-
-listhen.listen(h3.toNodeListener(app));
--- dist/lib/key.cjs
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const key = (event) => event.path;
-const headerKey = (event) => `${key(event)}-headers`;
-
-exports.headerKey = headerKey;
-exports.key = key;
